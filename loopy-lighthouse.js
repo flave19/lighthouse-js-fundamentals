ecb5f05cbf9e4f3de86823819276dd90f574136e
Flavian <Flavianmohanraj@gmail.com>
2019-12-04 21:33:43 +0000

    changes

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -4,7 +4,7 @@ If the number is a multiple of 3, print the string "Loopy" instead of the number
 If the number is a multiple of 4, print the string "Lighthouse" instead of the number.
 If the number is a multiple of both 3 and 4, print the string "LoopyLighthouse" instead of the number.*/
 
-for (i =100; i <=200; i++) {
+for (var i = 100; i <= 200; i++) {
   if (i % 3 === 0 && i % 4 === 0){
     console.log("LoopyLighthouse");
   }else if (i % 4 === 0){
